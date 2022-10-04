60306ff31e08c7c5e2eaa6a65895f8352aa0c688
BrinLewis <brinsley22@hotmail.co.uk>
2022-10-04 12:01:02 +0100

    Create and call playRound function

--- javascript.js
+++ javascript.js
@@ -1,13 +1,38 @@
 function getComputerChoice() {
   let num = Math.floor(Math.random() * 3);
   if (num === 0) {
-    return "Rock";
+    return "rock";
   } else if (num === 1) {
-    return "Paper";
+    return "paper";
   } else if (num === 2) {
-    return "Scissors"
+    return "scissors"
   } else {
-    return "Something has gone wrong!";
+    return "something has gone wrong!";
   }
   
 }
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    return "Draw! You both chose the same play!";
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "You Win! The computer chose scissors!";
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    return "You Lose! The computer chose paper!";
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return "You Win! The computer chose rock!";
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    return "You Lose! The computer chose scissors!"
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return "You Win! The computer chose paper!"
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    return "You Lose! The computer chose rock!"
+  } else {
+    return "You didn't choose a valid play."
+  }
+}
+
+let playerSelection = prompt("Enter your play: Rock, Paper or Scissors?");
+let computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
