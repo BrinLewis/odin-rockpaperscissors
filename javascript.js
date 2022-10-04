39d6e05b83500c2c003837b080bcb061a8c6f248
BrinLewis <brinsley22@hotmail.co.uk>
2022-10-04 16:37:22 +0100

    Create game function Change if...else statements in getComputerChoice to switch statement Create getPlayerChoice function Declare multiple variables for use in functions Edit playRound function to log results to console Change playRound return values to assign the roundWinner

--- javascript.js
+++ javascript.js
@@ -1,38 +1,68 @@
 function getComputerChoice() {
   let num = Math.floor(Math.random() * 3);
-  if (num === 0) {
-    return "rock";
-  } else if (num === 1) {
-    return "paper";
-  } else if (num === 2) {
-    return "scissors"
-  } else {
-    return "something has gone wrong!";
+  switch (num) {
+    case 0:
+      return "rock"
+    case 1:
+      return "paper"
+    case 2:
+      return "scissors"
   }
-  
 }
+
+function getPlayerChoice() {
+  return prompt("Enter your play: Rock, Paper or Scissors? First to 3 wins!");
+}
+
+let playerSelection = "none"
+let computerSelection = "none"
+let roundWinner = "nobody";
+let userScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
-    return "Draw! You both chose the same play!";
+    console.log("Draw! You both chose the same play!")
+    return roundWinner = "nobody";
   } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    return "You Win! The computer chose scissors!";
+    console.log("The computer chose scissors, +1 point to you!");
+    return roundWinner = "User";
   } else if (playerSelection == "rock" && computerSelection == "paper") {
-    return "You Lose! The computer chose paper!";
+    console.log("The computer chose paper, +1 point to them!");
+    return roundWinner = "Computer";
   } else if (playerSelection == "paper" && computerSelection == "rock") {
-    return "You Win! The computer chose rock!";
+    console.log("The computer chose rock, +1 point to you!");
+    return roundWinner = "User";
   } else if (playerSelection == "paper" && computerSelection == "scissors") {
-    return "You Lose! The computer chose scissors!"
+    console.log("The computer chose scissors, +1 point to them!");
+    return roundWinner = "Computer";
   } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    return "You Win! The computer chose paper!"
+    console.log("The computer chose paper, +1 point to you!");
+    return roundWinner = "User";
   } else if (playerSelection == "scissors" && computerSelection == "rock") {
-    return "You Lose! The computer chose rock!"
+    console.log("The computer chose rock, +1 point to them!");
+    return roundWinner = "Computer";
   } else {
-    return "You didn't choose a valid play."
+    console.log("You didn't choose a valid play.")
+    return roundWinner = "nobody";
   }
 }
 
-let playerSelection = prompt("Enter your play: Rock, Paper or Scissors?");
-let computerSelection = getComputerChoice();
+function game() {
+  for (let i = 0; i < 5; i++) {
+    playRound(getPlayerChoice(), getComputerChoice());
+  if (roundWinner == "User") {
+    userScore = ++userScore;
+  } else if (roundWinner == "Computer") {
+    computerScore = ++computerScore
+  }}
+  if (userScore > computerScore) {
+    return `Congratulations! You beat the computer ${userScore}:${computerScore}!`
+  } else if (userScore < computerScore) {
+    return `Damn! The computer beat you ${computerScore}:${userScore}!`
+  } else {
+    return `It's a draw! You drew ${userScore}:${computerScore}!`
+  }
+}
 
-console.log(playRound(playerSelection, computerSelection));
+console.log(game());
