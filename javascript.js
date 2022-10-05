df2effc0dc12efdd20c6c21d6eaeb237cf0e0ff5
BrinLewis <brinsley22@hotmail.co.uk>
2022-10-05 10:36:44 +0100

    Add live score tally when announcing round winner Make input case-insensitive

--- javascript.js
+++ javascript.js
@@ -11,7 +11,8 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-  return prompt("Enter your play: Rock, Paper or Scissors? First to 3 wins!");
+  let playerChoice = prompt("Enter your play: Rock, Paper or Scissors? First to 3 wins!");
+  return playerChoice.toLowerCase();
 }
 
 let playerSelection = "none"
@@ -22,25 +23,25 @@ let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
-    console.log("Draw! You both chose the same play!")
+    console.log(`Draw! You both chose the same play! You: ${userScore}, Computer: ${computerScore}`)
     return roundWinner = "nobody";
   } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    console.log("The computer chose scissors, +1 point to you!");
+    console.log(`The computer chose scissors, +1 point to you! You: ${userScore + 1}, Computer: ${computerScore}`);
     return roundWinner = "User";
   } else if (playerSelection == "rock" && computerSelection == "paper") {
-    console.log("The computer chose paper, +1 point to them!");
+    console.log(`The computer chose paper, +1 point to them! You: ${userScore}, Computer: ${computerScore + 1}`);
     return roundWinner = "Computer";
   } else if (playerSelection == "paper" && computerSelection == "rock") {
-    console.log("The computer chose rock, +1 point to you!");
+    console.log(`The computer chose rock, +1 point to you! You: ${userScore + 1}, Computer: ${computerScore}`);
     return roundWinner = "User";
   } else if (playerSelection == "paper" && computerSelection == "scissors") {
-    console.log("The computer chose scissors, +1 point to them!");
+    console.log(`The computer chose scissors, +1 point to them! You: ${userScore}, Computer: ${computerScore + 1}`);
     return roundWinner = "Computer";
   } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    console.log("The computer chose paper, +1 point to you!");
+    console.log(`The computer chose paper, +1 point to you! You: ${userScore + 1}, Computer: ${computerScore}`);
     return roundWinner = "User";
   } else if (playerSelection == "scissors" && computerSelection == "rock") {
-    console.log("The computer chose rock, +1 point to them!");
+    console.log(`The computer chose rock, +1 point to them! You: ${userScore}, Computer:${computerScore + 1}`);
     return roundWinner = "Computer";
   } else {
     console.log("You didn't choose a valid play.")
